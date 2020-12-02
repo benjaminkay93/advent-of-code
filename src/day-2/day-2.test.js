e4b45d3b0ce9bd50faf2ad4172810aec9bd5476d
Benjamin Kay <ben.kay01@hotmail.co.uk>
2020-12-02 23:10:29 +0000

    day 2

---
+++ src/day-2/day-2.test.js
@@ -0,0 +1,28 @@
+const {partOne, partTwo} = require('.');
+const { input } = require('./input');
+
+test('part 1', () => {
+  const testInput = `1-3 a: abcde
+    1-3 b: cdefg
+    2-9 c: ccccccccc`
+
+  expect(partOne(testInput)).toBe(2)
+})
+
+test('part 1 actual', () => {
+ 
+    expect(partOne(input)).toBe(454)
+  })
+
+test('part 2', () => {
+  const testInput = `1-3 a: abcde
+  1-3 b: cdefg
+  2-9 c: ccccccccc`
+  
+    expect(partTwo(testInput)).toBe(1)
+  })
+  
+  test('part 2 actual', () => {
+   
+      expect(partTwo(input)).toBe(649)
+    })
