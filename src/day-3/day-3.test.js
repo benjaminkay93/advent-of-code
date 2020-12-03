c1d727c1f4d9218c2149ca58069ece9040c732e5
benjaminkay93 <ben.kay01@hotmail.co.uk>
2020-12-03 08:49:32 +0000

    day 3 done :)

--- src/day-3/day-3.test.js
+++ src/day-3/day-3.test.js
@@ -1,26 +1,42 @@
 const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
-// test('part 1', () => {
-//   const testInput = `1-3 a: abcde
-//     1-3 b: cdefg
-//     2-9 c: ccccccccc`
+test('part 1', () => {
+  const testInput = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`
 
-//   expect(partOne(testInput)).toBe(2)
-// })
+  expect(partOne(testInput, 3, 1)).toBe(7)
+})
 
-// test('part 1 actual', () => {
-//   expect(partOne(input)).toBe(454)
-// })
+test('part 1 actual', () => {
+  expect(partOne(input, 3, 1)).toBe(195)
+})
 
-// test('part 2', () => {
-//   const testInput = `1-3 a: abcde
-//   1-3 b: cdefg
-//   2-9 c: ccccccccc`
+test('part 2', () => {
+  const testInput = `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`
 
-//   expect(partTwo(testInput)).toBe(1)
-// })
+  expect(partTwo(testInput)).toBe(336)
+})
 
-// test('part 2 actual', () => {
-//   expect(partTwo(input)).toBe(649)
-// })
+test('part 2 actual', () => {
+  expect(partTwo(input)).toBe(3772314000)
+})
--- src/day-3/index.js
+++ src/day-3/index.js
@@ -1,5 +1,37 @@
-const partOne = (input) => {}
+const TREE = '#'
 
-const partTwo = (input) => {}
+const partOne = (input, x, y) => {
+  const baseGrid = input.split('\n').map(line => line.split(''));
+
+  let pointerX = 0
+  let pointerY = 0
+  let treesEncountered = 0;
+
+  while (pointerY < baseGrid.length) {
+    if(baseGrid[pointerY][pointerX] === TREE)  treesEncountered++
+    pointerY = pointerY + y
+    pointerX = pointerX + x
+    if (baseGrid[0].length -1 < pointerX) {
+      pointerX = pointerX - baseGrid[0].length
+    }
+  }
+
+  return treesEncountered
+}
+
+const tests = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
+const partTwo = (input) => {
+  const results = tests.map(test => partOne(input, ...test));
+  return results.reduce((acc, val) => {
+    return acc*val
+  }, 1)
+}
 
 module.exports = { partOne, partTwo }
