8308058c80723fed117b549d28dba046a483140b
benjaminkay93 <ben.kay01@hotmail.co.uk>
2020-12-04 08:22:20 +0000

    day-4 complete :D

--- src/day-4/day-4.test.js
+++ src/day-4/day-4.test.js
@@ -1,20 +1,61 @@
 const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
-// test('part 1', () => {
-//   const testInput = ''
-//   expect(partOne(testInput)).toBe()
-// })
+test('part 1', () => {
+  const testInput = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
 
-// test('part 1 actual', () => {
-//   expect(partOne(input)).toBe()
-// })
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
 
-// test('part 2', () => {
-//   const testInput = ''
-//   expect(partTwo(testInput)).toBe()
-// })
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
 
-// test('part 2 actual', () => {
-//   expect(partTwo(input)).toBe()
-// })
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in`
+
+  expect(partOne(testInput)).toBe(2)
+})
+
+test('part 1 actual', () => {
+  expect(partOne(input)).toBe(219)
+})
+
+test('part 2 - invalid passports', () => {
+  const testInput = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`
+  expect(partTwo(testInput)).toBe(0)
+})
+
+test('part 2 - valid passports', () => {
+  const testInput = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`
+  expect(partTwo(testInput)).toBe(4)
+})
+
+test('part 2 actual', () => {
+  expect(partTwo(input)).toBe(127)
+})
--- src/day-4/index.js
+++ src/day-4/index.js
@@ -1,5 +1,133 @@
-const partOne = (input) => {}
+const parsePassport = (input) => input.split('\n\n')
 
-const partTwo = (input) => {}
+const requiredFields = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt', 'cid']
+
+const partOne = (input) => {
+  let validPassports = 0
+  parsePassport(input).forEach(passport => {
+    let validFields = 0
+    requiredFields.forEach(field => {
+      if (passport.includes(field)) {
+        validFields = validFields + 1
+      }
+    })
+
+    if (validFields === 8) {
+      validPassports = validPassports + 1
+      return
+    }
+
+    if (validFields === 7 && !passport.includes('cid')) {
+      validPassports = validPassports + 1
+    }
+  })
+  return validPassports
+}
+
+const parsePassportFields = passport => {
+  const passportObject = {}
+  passport.replace(/\n/g, ' ').split(' ').forEach(field => {
+    const [key, value] = field.split(':')
+    passportObject[key] = value
+  })
+
+  return passportObject
+}
+
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+const byr = value => {
+  if (value.length !== 4) return false
+  if (parseInt(value, 10) < 1920) return false
+  if (parseInt(value, 10) > 2002) return false
+  return true
+}
+
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+const iyr = value => {
+  if (value.length !== 4) return false
+  if (parseInt(value, 10) < 2010) return false
+  if (parseInt(value, 10) > 2020) return false
+  return true
+}
+
+// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+const eyr = value => {
+  if (value.length !== 4) return false
+  if (parseInt(value, 10) < 2020) return false
+  if (parseInt(value, 10) > 2030) return false
+  return true
+}
+// hgt (Height) - a number followed by either cm or in:
+// If cm, the number must be at least 150 and at most 193.
+// If in, the number must be at least 59 and at most 76.
+
+const hgt = value => {
+  if (value.includes('cm')) {
+    const [height] = value.split('cm')
+    if (height.length !== 3) return false
+    if (parseInt(height, 10) < 150) return false
+    if (parseInt(height, 10) > 193) return false
+    return true
+  }
+
+  if (value.includes('in')) {
+    const [height] = value.split('in')
+    if (height.length !== 2) return false
+    if (parseInt(height, 10) < 59) return false
+    if (parseInt(height, 10) > 76) return false
+    return true
+  }
+
+  return false
+}
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+
+const hcl = value => {
+  return /^#[0-9A-F]{6}$/i.test(value)
+}
+// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+const ecl = value => {
+  const validOptions = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+
+  return validOptions.includes(value)
+}
+// pid (Passport ID) - a nine-digit number, including leading zeroes.
+const pid = value => {
+  return /^[0-9]{9}$/i.test(value)
+}
+
+const validator = {
+  byr: byr,
+  iyr: iyr,
+  eyr: eyr,
+  hgt: hgt,
+  hcl: hcl,
+  ecl: ecl,
+  pid: pid,
+  cid: () => true
+}
+
+const partTwo = (input) => {
+  let validPassports = 0
+  const preparedPassports = parsePassport(input).map(parsePassportFields)
+
+  preparedPassports.map(passport => {
+    const keys = Object.keys(passport)
+
+    const invlaidFields = keys.filter(key => !validator[key](passport[key]))
+
+    if (keys.length === 8 && invlaidFields.length === 0) {
+      validPassports = validPassports + 1
+    }
+
+    if (keys.length === 7 && !keys.includes('cid') && invlaidFields.length === 0) {
+      validPassports = validPassports + 1
+    }
+
+    return invlaidFields
+  })
+
+  return validPassports;
+}
 
 module.exports = { partOne, partTwo }
