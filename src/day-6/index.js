c32ad14cd26d4b2cb6eda5506680dd3ec77032f6
benjaminkay93 <ben.kay01@hotmail.co.uk>
2020-12-06 08:42:39 +0000

    clean up intersection function

--- src/day-6/index.js
+++ src/day-6/index.js
@@ -1,19 +1,16 @@
 const intersection = (...args) => {
   const result = []
-  let lists
 
   if (args.length === 1) {
     return args[0]
-  } else {
-    lists = args
   }
 
-  for (let i = 0; i < lists.length; i++) {
-    const currentList = lists[i]
+  for (let i = 0; i < args.length; i++) {
+    const currentList = args[i]
     for (let y = 0; y < currentList.length; y++) {
       const currentValue = currentList[y]
       if (result.indexOf(currentValue) === -1) {
-        if (lists.filter(obj => obj.indexOf(currentValue) === -1).length == 0) {
+        if (args.filter(obj => obj.indexOf(currentValue) === -1).length === 0) {
           result.push(currentValue)
         }
       }
