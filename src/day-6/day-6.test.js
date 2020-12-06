85c31ef520ee714274e2c3aaae650c86133be7e7
benjaminkay93 <ben.kay01@hotmail.co.uk>
2020-12-06 08:40:55 +0000

    solve day-6

--- src/day-6/day-6.test.js
+++ src/day-6/day-6.test.js
@@ -2,21 +2,48 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `abc
 
-  expect(partOne(testInput)).toBe()
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`
+
+  expect(partOne(testInput)).toBe(11)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(6534)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `abc
 
-  expect(partTwo(testInput)).toBe()
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`
+  expect(partTwo(testInput)).toBe(6)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(3402)
 })
--- src/day-6/index.js
+++ src/day-6/index.js
@@ -1,5 +1,48 @@
-const partOne = (input) => {}
+const intersection = (...args) => {
+  const result = []
+  let lists
 
-const partTwo = (input) => {}
+  if (args.length === 1) {
+    return args[0]
+  } else {
+    lists = args
+  }
+
+  for (let i = 0; i < lists.length; i++) {
+    const currentList = lists[i]
+    for (let y = 0; y < currentList.length; y++) {
+      const currentValue = currentList[y]
+      if (result.indexOf(currentValue) === -1) {
+        if (lists.filter(obj => obj.indexOf(currentValue) === -1).length == 0) {
+          result.push(currentValue)
+        }
+      }
+    }
+  }
+
+  return result
+}
+
+const partOne = (input) => {
+  return input.split('\n\n').map(group => {
+    const prepGroup = group.replace(/\n/g, '').split('')
+    const set = new Set()
+
+    prepGroup.forEach(answer => set.add(answer))
+
+    return set.size
+  }).reduce((acc, val) => acc + val, 0)
+}
+
+const partTwo = (input) => {
+  return input.split('\n\n').map(group => {
+    const prepGroup = group.split('\n')
+
+    const prepAnswers = prepGroup.map(answer => answer.split(''))
+
+    const unibiquitousAnswers = intersection(...prepAnswers)
+    return unibiquitousAnswers.length
+  }).reduce((acc, val) => acc + val, 0)
+}
 
 module.exports = { partOne, partTwo }
