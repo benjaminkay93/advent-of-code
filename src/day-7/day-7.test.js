76b15d172b9ca2caebefc3c6ee04fde747ca6a46
benjaminkay93 <ben.kay01@hotmail.co.uk>
2020-12-07 08:10:27 +0000

    part 1 of day 7

--- src/day-7/day-7.test.js
+++ src/day-7/day-7.test.js
@@ -2,19 +2,34 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+deep teal bags contain 1 dark orange bags.`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(5)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(254)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(126)
 })
 
 test('part 2 actual', () => {
--- src/day-7/index.js
+++ src/day-7/index.js
@@ -1,4 +1,40 @@
-const partOne = (input) => {}
+const SEARCH_BAG = ['shiny gold']
+
+const fingHoldingBags = (input, searchTerm) => {
+  const inputKeys = Object.keys(input)
+
+  let containingArrays = []
+  searchTerm.forEach(search => inputKeys.forEach(key => {
+    //   console.log(searchTerm, search, ', ',key)
+    if (input[key].includes(search)) {
+      containingArrays.push(key)
+    }
+  }))
+
+  return containingArrays
+}
+
+const partOne = (input) => {
+  const parsedInput = input.split('\n').reduce((acc, bag) => {
+    const [hold, contents] = bag.split(' bags contain ')
+    return {
+      ...acc,
+      [hold]: contents
+    }
+  }, {})
+
+  const bags = new Set()
+
+  let result = fingHoldingBags(parsedInput, SEARCH_BAG)
+  result.forEach(individualResult => bags.add(individualResult))
+
+  do {
+    result = fingHoldingBags(parsedInput, result)
+    result.forEach(individualResult => bags.add(individualResult))
+  } while (result.length !== 0)
+
+  return bags.size
+}
 
 const partTwo = (input) => {}
 
