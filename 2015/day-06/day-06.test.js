5389a683ecdfd63c1bc855a1a966dfe7a53c4f9a
Benjamin Kay <ben.kay01@hotmail.co.uk>
2020-12-08 23:29:52 +0000

    WIP 2015 day-6

---
+++ 2015/day-06/day-06.test.js
@@ -0,0 +1,41 @@
+const { partOne, partTwo } = require('.')
+const { input } = require('./input')
+
+test('part 1', () => {
+  const testInput = `turn on 0,0 through 999,999`
+
+  expect(partOne(testInput)).toBe(1000000)
+})
+
+test('part 1', () => {
+  const testInput = `toggle 0,0 through 999,0`
+
+  expect(partOne(testInput)).toBe(1000)
+})
+
+test('part 1', () => {
+  const testInput = `turn off 499,499 through 500,500`
+
+  expect(partOne(testInput)).toBe(0)
+})
+
+test('part 1 2 lines', () => {
+  const testInput = `turn on 0,0 through 999,999
+turn off 499,499 through 500,500`
+
+  expect(partOne(testInput)).toBe(999996)
+})
+
+test('part 1 actual', () => {
+  expect(partOne(input)).toBe()
+})
+
+test('part 2', () => {
+  const testInput = ``
+
+  expect(partTwo(testInput)).toBe()
+})
+
+test('part 2 actual', () => {
+  expect(partTwo(input)).toBe()
+})
---
+++ 2015/day-06/index.js
@@ -0,0 +1,50 @@
+
+
+const parseInput = input => input.split('\n').map(line => {
+    if (line.includes('turn on')) {
+        return {
+            type: 'on',
+            area: line.replace('turn on ', '').split(' through ').map(xy => xy.split(','))
+        }
+    }
+    if (line.includes('turn off')) {
+        return {
+            type: 'off',
+            area: line.replace('turn off ', '').split(' through ').map(xy => xy.split(','))
+        }
+    }
+    if (line.includes('toggle')) {
+            const area = line.replace('toggle ', '').split(' through ').map(xy => xy.split(','))
+        return {
+            type: 'toggle',
+            area
+        }
+    }
+    console.log(line)
+});
+
+const partOne = (input) => {
+    const parsedInput = parseInput(input)
+
+    const grid = new Set()
+
+    const functions = {
+        on: (coordinates) => grid.add(coordinates),
+        off: (coordinates) => grid.delete(coordinates),
+        toggle: (coordinates) => grid.has(coordinates) ? grid.delete(coordinates) : grid.add(coordinates)
+    }
+
+    parsedInput.forEach(({type, area: [[startX, startY], [endX, endY]]}) => {
+        for(let x = startX; x <= endX; x++) {
+            for(let y = startY; y <= endY; y++) {
+                functions[type](`${x},${y}`)
+            }
+        }
+    })
+
+    return grid.size
+}
+
+const partTwo = (input) => {}
+
+module.exports = { partOne, partTwo }
