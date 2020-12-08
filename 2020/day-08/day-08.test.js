d70625ce113babc350844e7c8a18cfae45a28dc8
benjaminkay93 <ben.kay01@hotmail.co.uk>
2020-12-08 08:18:08 +0000

    day-08 2020 done :D

--- 2020/day-08/day-08.test.js
+++ 2020/day-08/day-08.test.js
@@ -2,21 +2,37 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(5)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(1610)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(8)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(1703)
 })
--- 2020/day-08/index.js
+++ 2020/day-08/index.js
@@ -1,5 +1,93 @@
-const partOne = (input) => {}
+const codeRunner = program => {
+  const visited = []
+  let position = 0
+  let accumulator = 0
+  let infinite = false
 
-const partTwo = (input) => {}
+  const instructionSet = {
+    nop: () => {
+      position++
+      execute()
+    },
+    acc: (modifier) => {
+      accumulator = accumulator + modifier
+      position++
+      execute()
+    },
+    jmp: (modifier) => {
+      position = position + modifier
+      execute()
+    }
+  }
+
+  const execute = () => {
+    if (visited.includes(position)) {
+      infinite = true
+      return
+    }
+
+    if (position === program.length) {
+      return
+    }
+    visited.push(position)
+
+    const { code, modifier } = program[position]
+
+    instructionSet[code](modifier)
+  }
+
+  execute()
+
+  return {
+    infinite,
+    accumulator
+  }
+}
+
+const partOne = (input) => {
+  const program = input.split('\n').map(input => {
+    const [code, modifier] = input.split(' ')
+    return {
+      code,
+      modifier: parseInt(modifier, 10)
+    }
+  })
+
+  const { accumulator } = codeRunner(program)
+
+  return accumulator
+}
+
+const partTwo = (input) => {
+  const baseProgram = input.split('\n').map(input => {
+    const [code, modifier] = input.split(' ')
+    return {
+      code,
+      modifier: parseInt(modifier, 10)
+    }
+  })
+  let found = false
+  let result
+
+  baseProgram.forEach((line, index) => {
+    if (found) return
+    const newProgram = baseProgram.map(newLine => ({ ...newLine }))
+
+    if (line.code === 'nop') {
+      newProgram[index].code = 'jmp'
+    }
+    if (line.code === 'jmp') {
+      newProgram[index].code = 'nop'
+    }
+
+    const ranResults = codeRunner(newProgram)
+    if (!ranResults.infinite) {
+      found = true
+      result = ranResults
+    }
+  })
+
+  return result.accumulator
+}
 
 module.exports = { partOne, partTwo }
