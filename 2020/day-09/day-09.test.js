2d2cdc4450a85ad2722af82cebdd4bf9351518e0
Benjamin Kay <ben.kay01@hotmail.co.uk>
2020-12-13 19:40:23 +0000

    WIP day 9

--- 2020/day-09/day-09.test.js
+++ 2020/day-09/day-09.test.js
@@ -2,21 +2,40 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput, 5)).toBe()
 })
 
-test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
-})
+// test('part 1 actual', () => {
+//   expect(partOne(input)).toBe()
+// })
 
-test('part 2', () => {
-  const testInput = ``
+// test('part 2', () => {
+//   const testInput = ``
 
-  expect(partTwo(testInput)).toBe()
-})
+//   expect(partTwo(testInput)).toBe()
+// })
 
-test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
-})
+// test('part 2 actual', () => {
+//   expect(partTwo(input)).toBe()
+// })
--- 2020/day-09/index.js
+++ 2020/day-09/index.js
@@ -1,4 +1,18 @@
-const partOne = (input) => {}
+const partOne = (input, preamble) => {
+  const parsedInput = input.split('\n')
+  let tempArray = []
+  let found = false;
+
+  parsedInput.forEach((item, index) => {
+    if (found) return
+    if (index === 0) return
+    
+    for ( let i = 0; i < preamble; i++) {
+        if (index-i < 0)return
+        const newArray = parsedInput.subarray(index-preamble, index-1)
+    }
+  })
+}
 
 const partTwo = (input) => {}
 
