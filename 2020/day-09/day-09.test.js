4fd644ca1f325f0b3e603868c66080d5e7f56b0d
Benjamin Kay <benjaminkay93@users.noreply.github.com>
2021-08-10 17:12:41 +0100

    day 9 of 2020 done :)

--- 2020/day-09/day-09.test.js
+++ 2020/day-09/day-09.test.js
@@ -23,19 +23,38 @@ test('part 1', () => {
 309
 576`
 
-  expect(partOne(testInput, 5)).toBe()
+  expect(partOne(testInput, 5)).toBe(127)
 })
 
-// test('part 1 actual', () => {
-//   expect(partOne(input)).toBe()
-// })
+test('part 1 actual', () => {
+  expect(partOne(input)).toBe(22406676)
+})
 
-// test('part 2', () => {
-//   const testInput = ``
+test('part 2', () => {
+  const testInput = `35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`
 
-//   expect(partTwo(testInput)).toBe()
-// })
+  expect(partTwo(testInput, 5)).toBe(62)
+})
 
-// test('part 2 actual', () => {
-//   expect(partTwo(input)).toBe()
-// })
+test('part 2 actual', () => {
+  expect(partTwo(input)).toBe(2942387)
+})
--- 2020/day-09/index.js
+++ 2020/day-09/index.js
@@ -1,19 +1,58 @@
-const partOne = (input, preamble) => {
-  const parsedInput = input.split('\n')
-  let tempArray = []
-  let found = false;
-
-  parsedInput.forEach((item, index) => {
-    if (found) return
-    if (index === 0) return
-    
-    for ( let i = 0; i < preamble; i++) {
-        if (index-i < 0)return
-        const newArray = parsedInput.subarray(index-preamble, index-1)
+const prepareArray = (rawInput, preamble) => {
+  const numbersToCheck = rawInput.slice(preamble)
+  return numbersToCheck.map((val, index) => {
+    return {
+      val,
+      subArray: rawInput.slice(index, index + preamble)
     }
   })
 }
 
-const partTwo = (input) => {}
+const arrayOfAdditions = (array, escapeValue) => {
+  const [valueToCheck, ...subArray] = array || []
+  if (subArray.length === 0) return []
+  const newArray = subArray.reduce((acc, val) => {
+    return [...acc, val + valueToCheck]
+  }, [])
+
+  return [...newArray, ...arrayOfAdditions(subArray, escapeValue)]
+}
+
+const partOne = (input, preamble = 25) => {
+  const parsedInput = input.split('\n').map(num => Number(num))
+  const array = prepareArray(parsedInput, preamble)
+  let done = false
+  return array.map(value => {
+    if (done) return true
+    const allSums = arrayOfAdditions(value.subArray, value.val)
+    if (allSums.includes(value.val)) {
+      return true
+    }
+    done = true
+    return value.val
+  }).filter(value => typeof value === 'number')[0]
+}
+
+const partTwo = (input, preamble = 25) => {
+  const parsedInput = input.split('\n').map(num => Number(num))
+  const expectedResult = partOne(input, preamble)
+
+  const arrayLocation = parsedInput.map((val, index) => {
+    return parsedInput.slice(index).reduce((acc, subVal, subIndex) => {
+      if (typeof acc === 'object') return acc
+      if (typeof acc === 'string') return 'bust'
+      if (acc > expectedResult) return 'bust'
+      if (acc === expectedResult) return { index, subIndex }
+      return acc + subVal
+    }, 0)
+  }).filter(val => typeof val === 'object')[0]
+
+  const finalArray = parsedInput.slice(arrayLocation.index, arrayLocation.subIndex + arrayLocation.index)
+
+  const sortedFinalArray = finalArray.sort((a, b) => a - b)
+  const start = sortedFinalArray[0]
+  const end = sortedFinalArray.reverse()[0]
+  return start + end
+}
 
 module.exports = { partOne, partTwo }
