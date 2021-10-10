acd3069067edd7bfa01b5e8cc7c09235956d7de1
benjaminkay93 <ben.kay01@hotmail.co.uk>
2021-10-10 11:31:49 +0100

    generate 2021

---
+++ scripts/generate-year-template.mjs
@@ -0,0 +1,30 @@
+import { mkdirSync, existsSync, copyFileSync } from 'fs'
+import path from 'path'
+
+const __dirname = path.resolve()
+
+const indexTemplate = path.join(__dirname, 'template', 'index.js')
+const inputTemplate = path.join(__dirname, 'template', 'input.js')
+const testemplate = path.join(__dirname, 'template', 'day-n.test.js')
+
+const generateFolders = (year) => {
+  const dirBase = path.join(__dirname, 'years', year)
+  for (let i = 1; i <= 25; i++) {
+    const date = i < 10 ? `${i}`.padStart(2, '0') : i
+    const folderPath = path.join(dirBase, `day-${date}`)
+    if (!existsSync(folderPath)) {
+      mkdirSync(folderPath, { recursive: true })
+      copyFileSync(indexTemplate, path.join(folderPath, 'index.js'))
+      copyFileSync(inputTemplate, path.join(folderPath, 'input.js'))
+      copyFileSync(testemplate, path.join(folderPath, `day-${date}.test.js`))
+    }
+  }
+}
+
+const generateYear = (year) => {
+  generateFolders(year)
+}
+
+const yearToGenerate = process.argv[2]
+
+generateYear(yearToGenerate || '2030')
