468b586a3a2b9bf7ac242cca9fccbb392f530a7e
Benjamin Kay <benjaminkay93@users.noreply.github.com>
2021-12-06 19:11:18 +0000

    day 13 2020

--- years/2020/day-13/day-13.test.js
+++ years/2020/day-13/day-13.test.js
@@ -12,12 +12,41 @@ test('part 1 actual', () => {
   expect(partOne(input)).toBe(296)
 })
 
-test('part 2', () => {
-  const testInput = ``
+test('part 2 simple', () => {
+  const testInput = `1
+67,7,59,61`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(754018)
+})
+
+test('part 2 simple with x', () => {
+  const testInput = `1
+67,x,7,59,61`
+
+  expect(partTwo(testInput)).toBe(779210)
+})
+
+test('part 2 simple with x in different place', () => {
+  const testInput = `1
+67,7,x,59,61`
+
+  expect(partTwo(testInput)).toBe(1261476)
+})
+
+test('part 2 large', () => {
+  const testInput = `1
+1789,37,47,1889`
+
+  expect(partTwo(testInput)).toBe(1202161486)
+})
+
+test('part 2 complex', () => {
+  const testInput = `939
+7,13,x,x,59,x,31,19`
+
+  expect(partTwo(testInput)).toBe(1068781)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(535296695251210)
 })
--- years/2020/day-13/index.js
+++ years/2020/day-13/index.js
@@ -1,3 +1,5 @@
+const ts = require("typescript")
+
 const nextBusDepart = (startingTs, busIds) => {
   const timeToNextBus = busIds.map(id => id - (startingTs % id))
 
@@ -20,6 +22,78 @@ const partOne = (input) => {
   return timeUntilBus * busId
 }
 
-const partTwo = (input) => {}
+// const partTwo = (input) => {
+//   const [, rawBusIds] = input.split('\n')
+//   const busIds = rawBusIds.split(',').map(val => {
+//     if (val === 'x') return 'x'
+//     return parseInt(val, 10)
+//   })
+
+//   console.log(busIds)
+
+//   let converganceTs = busIds[0]
+//   let successful = false
+
+//   while (!successful) {
+//     converganceTs = converganceTs + busIds[0]
+//     successful = busIds.reduce((validator, value, index) => {
+//       if (!validator) return validator
+//       if (value === 'x') return validator
+//       return (converganceTs + index) % value === 0
+//     }, true)
+//   }
+
+//   return converganceTs
+// }
+
+// safe with negative numbers unlike JS % operator
+const absoluteModulo = (a, b) => ((a % b) + b) % b
+
+const getInverse = (a, mod) => {
+  const b = a % mod
+  for (let i = 1; i < mod; i++) {
+    if ((b * i) % mod === 1) {
+      return i
+    }
+  }
+  return 1
+}
+
+const chineseRemainderTheorem = (lines) => {
+  // x =- a (mod n)
+  // x - some unknown, constant value of t
+  // a - bus number MINUS offset % bus number
+  // n - cycle length (= bus number)
+
+  // to solve each row, we also need
+  // N - all n's added up
+  // nU = N / n
+  // i - inverse modulo
+
+  const N = lines.reduce((acc, val) => {
+    if (val === "x") return acc
+    return acc === null ? val : acc * val
+  }, null)
+
+  const sum = lines.reduce((acc, val, idx) => {
+    if (val === "x") {
+      return acc
+    }
+    const a = absoluteModulo(val - idx, val)
+    const nU = N / val
+    const inverse = getInverse(nU, val)
+
+    return acc + BigInt(BigInt(a) * BigInt(nU) * BigInt(inverse))
+  }, 0n)
+
+  return sum % BigInt(N)
+}
+
+// Credit to https://codesandbox.io/s/focused-lewin-subsf?file=/src/index.js:0-1854 for having a better way of doing this than my first one <3
+const partTwo = (input) => {
+  const [, rawBusIds] = input.split("\n")
+  const busIds = rawBusIds.split(',').map(val => val === 'x' ? 'x' : parseInt(val, 10))
+  return parseInt(`${chineseRemainderTheorem(busIds)}`, 10)
+}
 
 module.exports = { partOne, partTwo }
