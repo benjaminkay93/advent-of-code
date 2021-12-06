78d23880c78382c6a1069ff91be828be4a2f8b4e
Benjamin Kay <benjaminkay93@users.noreply.github.com>
2021-12-06 11:46:57 +0000

    day 5 done

--- years/2021/day-05/day-05.test.js
+++ years/2021/day-05/day-05.test.js
@@ -2,21 +2,54 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(5)
+})
+
+test('part 1 invertedY', () => {
+  const testInput = `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,4 -> 7,0
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`
+
+  expect(partOne(testInput)).toBe(5)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(5145)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(12)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(16518)
 })
--- years/2021/day-05/index.js
+++ years/2021/day-05/index.js
@@ -1,5 +1,113 @@
-const partOne = (input) => {}
+const UNKNOWN = 'unknown';
+const X = 'x';
+const Y = 'y';
 
-const partTwo = (input) => {}
+function* range(from, to) {
+	const stepValue = to > from ? 1 : -1;
+	let totalSteps = Math.abs(from - to) + 1;
+
+	let value = from;
+	while (totalSteps--) {
+		yield value;
+		value += stepValue;
+	}
+}
+
+function* diagonalRange(x0, y0, x1, y1) {
+	let stepValueX = x1 === x0 ? 0 : x1 > x0 ? 1 : -1;
+	let stepValueY = y1 === y0 ? 0 : y1 > y0 ? 1 : -1;
+	let totalStepsX = Math.abs(x0 - x1) + 1;
+	let totalStepsY = Math.abs(y0 - y1) + 1;
+
+	let totalSteps = Math.max(totalStepsX, totalStepsY);
+
+	let x = x0;
+	let y = y0;
+	while (totalSteps--) {
+		yield [x, y];
+		x += stepValueX;
+		y += stepValueY;
+	}
+}
+
+const getLines = (array) => array.map(line => {
+  return line.split(' -> ').reduce((acc, val, index) => {
+    const [x, y] = val.split(',')
+
+    let direction = UNKNOWN
+
+    if (index === 0) {
+      return {
+        x0: parseInt(x, 10),
+        y0: parseInt(y, 10),
+        direction
+      }
+    }
+
+    if (index === 1) {
+      if (y == acc.y0) direction = X;
+      if (x == acc.x0) direction = Y;
+
+      return {
+        ...acc,
+        x1: parseInt(x, 10),
+        y1: parseInt(y, 10),
+        direction
+      }
+    }
+  }, {})
+})
+
+const partOne = (input) => {
+  const lines = getLines(input.split('\n')).filter(line => line.direction !== UNKNOWN)
+
+  const intersections = {};
+
+  lines.forEach(({x0, y0, x1, y1, direction}) => {
+    if (direction === X) {
+      for (let x of range(x0, x1)) {
+        let count = intersections[`${x},${y1}`] || 0
+        intersections[`${x},${y1}`] = count + 1
+      }
+    }
+    if (direction === Y) {
+      for (let y of range(y0, y1)) {
+        let count = intersections[`${x1},${y}`] || 0
+        intersections[`${x1},${y}`] = count + 1
+      }
+    }
+  })
+
+  return Object.values(intersections).filter(val => val > 1).length
+}
+
+const partTwo = (input) => {
+  const lines = getLines(input.split('\n'))
+
+  const intersections = {};
+
+  lines.forEach(({x0, y0, x1, y1, direction}) => {
+    if (direction === X) {
+      for (let x of range(x0, x1)) {
+        let count = intersections[`${x},${y1}`] || 0
+        intersections[`${x},${y1}`] = count + 1
+      }
+    }
+    if (direction === Y) {
+      for (let y of range(y0, y1)) {
+        let count = intersections[`${x1},${y}`] || 0
+        intersections[`${x1},${y}`] = count + 1
+      }
+    }
+    if (direction === UNKNOWN) {
+      for (let [x, y] of diagonalRange(x0, y0, x1, y1)) {
+        let count = intersections[`${x},${y}`] || 0
+        intersections[`${x},${y}`] = count + 1
+      }
+    }
+  })
+
+  return Object.values(intersections).filter(val => val > 1).length
+}
 
 module.exports = { partOne, partTwo }
