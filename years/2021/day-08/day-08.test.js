1c17a4e3df2f7ab251b4914c1c1755c68ed31aab
benjaminkay93 <github@bkay.uk>
2022-06-24 21:00:21 +0300

    day-08 part 1

--- years/2021/day-08/day-08.test.js
+++ years/2021/day-08/day-08.test.js
@@ -2,13 +2,22 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+  edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+  fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+  fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+  aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+  fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+  dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+  bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+  egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+  gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(26)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(375)
 })
 
 test('part 2', () => {
--- years/2021/day-08/index.js
+++ years/2021/day-08/index.js
@@ -1,4 +1,29 @@
-const partOne = (input) => {}
+const segmentCount = {
+  0: 6,
+  1: 2,
+  2: 5,
+  3: 5,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 3,
+  8: 7,
+  9: 6,
+}
+
+const partOne = (input) => {
+  const outputValues = input.split('\n').map(line => line.split(' | ')[1])
+  return outputValues.reduce((acc, line) => {
+    let countLine = 0;
+    line.split(' ').forEach(output => {
+      if (output.length === segmentCount[1]) countLine += 1
+      if (output.length === segmentCount[4]) countLine += 1
+      if (output.length === segmentCount[7]) countLine += 1
+      if (output.length === segmentCount[8]) countLine += 1
+    })
+    return acc + countLine;
+  }, 0)
+}
 
 const partTwo = (input) => {}
 
