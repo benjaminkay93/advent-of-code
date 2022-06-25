d0ab7cad7b0093689df2f0a3331a25c72178bd82
benjaminkay93 <github@bkay.uk>
2022-06-25 14:51:54 +0300

    completed part 1

--- years/2021/day-13/day-13.test.js
+++ years/2021/day-13/day-13.test.js
@@ -2,19 +2,59 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
 
-  expect(partOne(testInput)).toBe()
+fold along y=7
+fold along x=5`
+
+  expect(partOne(testInput)).toBe(17)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(753)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
 
-  expect(partTwo(testInput)).toBe()
+fold along y=7
+fold along x=5`
+
+  expect(partTwo(testInput)).toBe(0)
 })
 
 test('part 2 actual', () => {
--- years/2021/day-13/index.js
+++ years/2021/day-13/index.js
@@ -1,5 +1,49 @@
-const partOne = (input) => {}
+const foldY = (grid, foldNumber) => {
+  return grid.map(dot => {
+    const [x, y] = dot.split(',').map(value => parseInt(value, 10))
+    if (y < foldNumber) return dot;
+    const newPosition = `${x},${foldNumber - (y - foldNumber)}`
+    if(grid.includes(newPosition)) return null;
+    return newPosition
+  }).filter(dot => dot)
+}
 
-const partTwo = (input) => {}
+const foldX = (grid, foldNumber) => {
+  return grid.map(dot => {
+    const [x, y] = dot.split(',').map(value => parseInt(value, 10))
+    if (x < foldNumber) return dot;
+    const newPosition = `${foldNumber - (x - foldNumber)},${y}`
+    if(grid.includes(newPosition)) return null;
+    return newPosition
+  }).filter(dot => dot)
+}
+const strategy = {
+  x: foldX,
+  y: foldY,
+}
+
+const partOne = (input) => {
+  const [rawStart, rawInstructions ] = input.split('\n\n')
+  let grid = rawStart.split('\n')
+  const instructions = rawInstructions.split('\n').map(rawItem => rawItem.replace('fold along ','').split('='))
+  const setToRun = [instructions[0]]
+  setToRun.forEach(([direction, foldNumber]) => {
+    grid = strategy[direction](grid, foldNumber)
+  })
+
+  return grid.length
+}
+
+const partTwo = (input) => {
+  const [rawStart, rawInstructions ] = input.split('\n\n')
+  let grid = rawStart.split('\n')
+  const instructions = rawInstructions.split('\n').map(rawItem => rawItem.replace('fold along ','').split('='))
+
+  instructions.forEach(([direction, foldNumber]) => {
+    grid = strategy[direction](grid, foldNumber)
+  })
+
+  return grid.length
+}
 
 module.exports = { partOne, partTwo }
