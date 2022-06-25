c6a1a77b7dc68d3cf2e578a255603085887c2172
benjaminkay93 <github@bkay.uk>
2022-06-25 13:48:51 +0300

    expand tests for day 12

--- years/2021/day-12/day-12.test.js
+++ years/2021/day-12/day-12.test.js
@@ -1,10 +1,54 @@
 const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
-test('part 1', () => {
-  const testInput = ``
+test('part 1a', () => {
+  const testInput = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(10)
+})
+
+test('part 1b', () => {
+  const testInput = `dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc`
+
+  expect(partOne(testInput)).toBe(19)
+})
+
+test('part 1c', () => {
+  const testInput = `fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`
+
+  expect(partOne(testInput)).toBe(226)
 })
 
 test('part 1 actual', () => {
