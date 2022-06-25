c7f75a2dd1d4a93687671bdd3d8a277ed62addbe
benjaminkay93 <github@bkay.uk>
2022-06-25 17:43:46 +0300

    rudimentary approach done, move to dictionary now

--- years/2021/day-14/day-14.test.js
+++ years/2021/day-14/day-14.test.js
@@ -2,19 +2,53 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `NNCB
 
-  expect(partOne(testInput)).toBe()
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`
+
+  expect(partOne(testInput)).toBe(1588)
 })
 
 test('part 1 actual', () => {
   expect(partOne(input)).toBe()
 })
 
-test('part 2', () => {
-  const testInput = ``
+test.only('part 2', () => {
+  const testInput = `NNCB
 
-  expect(partTwo(testInput)).toBe()
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`
+
+  expect(partTwo(testInput)).toBe(2188189693529)
 })
 
 test('part 2 actual', () => {
--- years/2021/day-14/index.js
+++ years/2021/day-14/index.js
@@ -1,5 +1,47 @@
-const partOne = (input) => {}
+const partOne = (input, steps = 10) => {
+  const [string, rawRules] = input.split('\n\n')
+  const rules = {}
+  rawRules.split('\n').map(line => line.split(' -> ')).forEach(([rule, newChar]) => {
+    const [first, last] = rule.split('')
+    rules[rule] = newChar
+  })
 
-const partTwo = (input) => {}
+  let oldString = string
+
+  const step = () => {
+    const stringArray = oldString.split('')
+    
+    let newString = ''
+    
+    stringArray.forEach((char, index) => {
+      if (index + 1 === stringArray.length) {
+        newString = newString + char
+        return
+      }
+      newString = newString + char + rules[`${char}${stringArray[index + 1]}`]
+    })
+    oldString = newString
+  }
+
+  let i = 0;
+  while ( i < steps) {
+    step()
+    i++
+    console.log('step', i)
+  }
+
+  const count = {}
+
+  oldString.split('').forEach(char => {
+    if(!count?.[char]) count[char] = 0
+    count[char] += 1
+  })
+
+  const sortedCount = Object.values(count).sort((a, b) => a - b)
+
+  return sortedCount[sortedCount.length - 1] - sortedCount[0]
+}
+
+const partTwo = (input) => partOne(input, 40)
 
 module.exports = { partOne, partTwo }
