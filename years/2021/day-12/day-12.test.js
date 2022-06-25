80bf9798ab7d843b9edc69108a2a23f06838862c
benjaminkay93 <github@bkay.uk>
2022-06-25 14:21:27 +0300

    day 12 done

--- years/2021/day-12/day-12.test.js
+++ years/2021/day-12/day-12.test.js
@@ -52,15 +52,60 @@ start-RW`
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(3576)
 })
 
-test('part 2', () => {
-  const testInput = ``
 
-  expect(partTwo(testInput)).toBe()
+test('part 2a', () => {
+  const testInput = `start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end`
+
+  expect(partTwo(testInput)).toBe(36)
+})
+
+test('part 2b', () => {
+  const testInput = `dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc`
+
+  expect(partTwo(testInput)).toBe(103)
+})
+
+test('part 2c', () => {
+  const testInput = `fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW`
+
+  expect(partTwo(testInput)).toBe(3509)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(84271)
 })
--- years/2021/day-12/index.js
+++ years/2021/day-12/index.js
@@ -1,5 +1,74 @@
-const partOne = (input) => {}
+const isSmallCave = (cave) => cave.match(/^[a-z]+$/);
 
-const partTwo = (input) => {}
+const findPathsPartOne = ({startPosition, endPosition, map, paths, currentPath}) => {
+	currentPath[startPosition] = currentPath[startPosition] + 1 || 1;
+
+  if (!map[startPosition]) return;
+	if (startPosition === endPosition) {
+		paths.push(currentPath);
+		return;
+	}
+
+	for (const position of map[startPosition]) {
+		if (isSmallCave(position) && currentPath?.[position]) {
+			continue;
+		}
+		findPathsPartOne({startPosition: position, endPosition, map, paths, currentPath: {...currentPath}});
+	}
+
+	return paths;
+}
+
+const partOne = (input) => {
+	const map = {};
+
+  input.split('\n').forEach(line => {
+    const [x, y] = line.split('-');
+
+    map[x] ? map[x].push(y) : map[x] = [y];
+    map[y] ? map[y].push(x) : map[y] = [x];
+  })
+
+  return findPathsPartOne({startPosition: 'start', endPosition: 'end', map, paths: [], currentPath: {}}).length
+}
+
+
+const findPathsPartTwo = ({map, startPosition, endPosition, paths, currentPath}) => {
+	currentPath[startPosition] = currentPath[startPosition] + 1 || 1;
+
+	if (isSmallCave(startPosition) && currentPath[startPosition] === 2) {
+		currentPath['visited twice'] = true;
+	}
+  if (!map[startPosition]) return;
+	if (startPosition === endPosition) {
+		paths.push(currentPath);
+		return;
+	}
+
+
+	for (const position of map[startPosition]) {
+		if (isSmallCave(position) && position in currentPath) {
+			if (['start', 'end'].includes(position) || currentPath['visited twice']) {
+				continue;
+			}
+		}
+		findPathsPartTwo({startPosition: position, endPosition, map, paths, currentPath: {...currentPath}});
+	}
+
+	return paths;
+}
+
+const partTwo = (input) => {
+	const map = {};
+
+  input.split('\n').forEach(line => {
+    const [x, y] = line.split('-');
+
+    map[x] ? map[x].push(y) : map[x] = [y];
+    map[y] ? map[y].push(x) : map[y] = [x];
+  })
+
+  return findPathsPartTwo({startPosition: 'start', endPosition: 'end', map, paths: [], currentPath: {}}).length
+}
 
 module.exports = { partOne, partTwo }
