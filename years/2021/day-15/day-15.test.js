4e9d91f1990c2169729e250e77f0e2c33230d2bc
benjaminkay93 <github@bkay.uk>
2022-06-25 22:22:15 +0300

    set up for tomorrow, need to relearn djikstars

--- years/2021/day-15/day-15.test.js
+++ years/2021/day-15/day-15.test.js
@@ -1,10 +1,19 @@
 const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
-test('part 1', () => {
-  const testInput = ``
+test.only('part 1', () => {
+  const testInput = `1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(40)
 })
 
 test('part 1 actual', () => {
