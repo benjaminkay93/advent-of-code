dc1a94e09516cd8e97550790e6c19c2990c04af1
benjaminkay93 <github@bkay.uk>
2022-06-25 13:31:08 +0300

    day 11 part 1

--- years/2021/day-11/day-11.test.js
+++ years/2021/day-11/day-11.test.js
@@ -1,14 +1,43 @@
 const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
-test('part 1', () => {
-  const testInput = ``
+test('part 1a', () => {
+  const testInput = `11111
+19991
+19191
+19991
+11111`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput, 1)).toBe(9)
+})
+
+test('part 1b', () => {
+  const testInput = `11111
+19991
+19191
+19991
+11111`
+
+  expect(partOne(testInput, 2)).toBe(9)
+})
+
+test('part 1', () => {
+  const testInput = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`
+
+  expect(partOne(testInput)).toBe(1656)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(1747)
 })
 
 test('part 2', () => {
--- years/2021/day-11/index.js
+++ years/2021/day-11/index.js
@@ -1,4 +1,77 @@
-const partOne = (input) => {}
+const humanReadable = grid => {
+  let array = []
+  Object.keys(grid).forEach(position => {
+    const [x, y] = position.split(',').map(value => parseInt(value, 10))
+    if (!array?.[x]) {
+      array.push([grid[position]])
+      return
+    }
+    array[x].push(grid[position])
+  })
+
+  return array.map(line => line.join('')).join('\n')
+}
+
+const partOne = (input, stepCount = 100) => {
+  const grid = {}
+  const flashed = []
+  input.split('\n').forEach((line, xIndex) => line.split('').forEach((number, yIndex) => grid[`${xIndex},${yIndex}`] = parseInt(number, 10)))
+
+  const step = () => {
+    const visited = new Set()
+    Object.keys(grid).forEach(position => {
+      grid[position] += 1
+    })
+    
+    const increment = position => {
+      if (visited.has(position)) return
+      if(grid[position] > 9) {
+        visited.add(position)
+        const [x, y] = position.split(',').map(value => parseInt(value, 10))
+        const topLeft = `${x+1},${y+1}`
+        const top = `${x},${y+1}`
+        const topRight = `${x-1},${y+1}`
+        const left = `${x-1},${y}`
+        const right = `${x+1},${y}`
+        const bottomLeft = `${x-1},${y-1}`
+        const bottom = `${x},${y-1}`
+        const bottomRight = `${x+1},${y-1}`
+        
+        guardAndIncrement(topLeft)
+        guardAndIncrement(top)
+        guardAndIncrement(topRight)
+        guardAndIncrement(left)
+        guardAndIncrement(right)
+        guardAndIncrement(bottomLeft)
+        guardAndIncrement(bottom)
+        guardAndIncrement(bottomRight)
+      }
+    }
+
+    const guardAndIncrement = (position) => {
+      if(grid?.[position] && !visited.has(position)) {
+        grid[position] += 1
+        increment(position)
+      }
+    }
+
+    Object.keys(grid).forEach(increment)
+    
+    Object.keys(grid).forEach(position => {
+      if(grid[position] > 9) {
+        flashed.push(position)
+        grid[position] = 0
+      }
+    })
+  }
+  let i = 0
+  while (i < stepCount) {
+    step()
+    i++;
+  }
+    
+  return flashed.length
+}
 
 const partTwo = (input) => {}
 
