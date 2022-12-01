5b829181d570cbff2cd3498b1c0c74d97cd163e7
benjaminkay93 <github@bkay.uk>
2022-12-01 10:26:21 +0000

    day 1 done

--- years/2022/day-01/day-01.test.js
+++ years/2022/day-01/day-01.test.js
@@ -2,19 +2,45 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `1000
+2000
+3000
 
-  expect(partOne(testInput)).toBe()
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+
+  expect(partOne(testInput)).toBe(24000)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(72240)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `1000
+2000
+3000
 
-  expect(partTwo(testInput)).toBe()
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`
+
+  expect(partTwo(testInput)).toBe(45000)
 })
 
 test('part 2 actual', () => {
--- years/2022/day-01/index.js
+++ years/2022/day-01/index.js
@@ -1,5 +1,23 @@
-const partOne = (input) => {}
+const partOne = (input) => {
+  const totalCaloryCount = input.split('\n\n').map(elf => {
+    return elf.split('\n').reduce((acc, val) => {
+      return parseInt(val, 10) + acc
+    }, 0)
+  })
 
-const partTwo = (input) => {}
+  const sortedCount = totalCaloryCount.sort((a, b) => b - a)
+  return sortedCount[0]
+}
+
+const partTwo = (input) => {
+  const totalCaloryCount = input.split('\n\n').map(elf => {
+    return elf.split('\n').reduce((acc, val) => {
+      return parseInt(val, 10) + acc
+    }, 0)
+  })
+
+  const sortedCount = totalCaloryCount.sort((a, b) => b - a)
+  return sortedCount[0] + sortedCount[1] + sortedCount[2]
+}
 
 module.exports = { partOne, partTwo }
