ade293738f52b970f5167deda25ac0a24394d7b8
Benjamin Kay <github@bkay.uk>
2023-12-01 17:54:53 +0000

    2023 day 1 done

--- years/2023/day-01/day-01.test.js
+++ years/2023/day-01/day-01.test.js
@@ -2,21 +2,35 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(142)
+})
+test('part 1 - number only at end', () => {
+  const testInput = `abc2`
+
+  expect(partOne(testInput)).toBe(22)
+})
+
+test('part 1 - number only at start', () => {
+  const testInput = `2abc`
+
+  expect(partOne(testInput)).toBe(22)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(55130)
 })
 
-test('part 2', () => {
-  const testInput = ``
+// test('part 2', () => {
+//   const testInput = ``
 
-  expect(partTwo(testInput)).toBe()
-})
+//   expect(partTwo(testInput)).toBe()
+// })
 
-test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
-})
+// test('part 2 actual', () => {
+//   expect(partTwo(input)).toBe()
+// })
--- years/2023/day-01/index.js
+++ years/2023/day-01/index.js
@@ -1,4 +1,23 @@
-const partOne = (input) => {}
+const decimalMatcher = /\d/
+const partOne = (input) => {
+  const count = input.split('\n').map(code => {
+    const firstLastNumber = code.split('').reduce((acc, val, index) => {
+      if (!decimalMatcher.test(val)) {
+        if (index === code.length - 1 && !acc.last) {
+          return { ...acc, last: acc.first }
+        }
+        return acc
+      }
+      if (!acc.first && index === code.length - 1) return { first: val, last: val }
+      if (!acc.first) return { first: val, last: null }
+      return { first: acc.first, last: val }
+    }, { first: null, last: null })
+    return Number(`${firstLastNumber.first}${firstLastNumber.last}`)
+  }).reduce((acc, val) => {
+    return acc + val
+  }, 0)
+  return count
+}
 
 const partTwo = (input) => {}
 
