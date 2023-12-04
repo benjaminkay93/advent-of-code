f9640f5b9cd28ff3c192d1514384208f9c9d7187
Benjamin Kay <github@bkay.uk>
2023-12-04 12:36:08 +0000

    2023 day 2 part 1

--- years/2023/day-02/day-02.test.js
+++ years/2023/day-02/day-02.test.js
@@ -2,21 +2,25 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(8)
 })
 
 test('part 1 actual', () => {
   expect(partOne(input)).toBe()
 })
 
-test('part 2', () => {
-  const testInput = ``
+// test('part 2', () => {
+//   const testInput = ``
 
-  expect(partTwo(testInput)).toBe()
-})
+//   expect(partTwo(testInput)).toBe()
+// })
 
-test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
-})
+// test('part 2 actual', () => {
+//   expect(partTwo(input)).toBe()
+// })
--- years/2023/day-02/index.js
+++ years/2023/day-02/index.js
@@ -1,4 +1,44 @@
-const partOne = (input) => {}
+const partOneTarget = {
+  red: 12,
+  green: 13,
+  blue: 14,
+}
+
+const partOne = (input) => {
+  const games = input.split('\n').map((line, index) => {
+    const [, gameRaw] = line.split(': ')
+    const gameId = index + 1
+
+    const gameParsed = gameRaw.split('; ').map(game => {
+      return game.split(', ').reduce((acc, set) => {
+        const [actColor, value] = set.split(' ').reverse()
+        return {
+          ...acc,
+          [actColor]: Number(value),
+        }
+      }, {})
+    })
+    return {
+      gameId,
+      sets: gameParsed,
+    }
+  })
+
+  const filteredGames = games.filter((game) => {
+    const doesBreakRule = game.sets.reduce((acc, set) => {
+      if (acc === true) return true
+      if (set.red > partOneTarget.red) return true
+      if (set.green > partOneTarget.green) return true
+      if (set.blue > partOneTarget.blue) return true
+      return false
+    }, false)
+    return !doesBreakRule
+  }).reduce((acc, val) => {
+    return acc + val.gameId
+  }, 0)
+
+  return filteredGames
+}
 
 const partTwo = (input) => {}
 
