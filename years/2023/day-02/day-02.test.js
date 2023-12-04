7d12fd9530988fc6b52ca926876db5619114996b
Benjamin Kay <github@bkay.uk>
2023-12-04 12:46:25 +0000

    2023 day 2 part 2

--- years/2023/day-02/day-02.test.js
+++ years/2023/day-02/day-02.test.js
@@ -12,15 +12,19 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(2204)
 })
 
-// test('part 2', () => {
-//   const testInput = ``
+test('part 2', () => {
+  const testInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
-//   expect(partTwo(testInput)).toBe()
-// })
+  expect(partTwo(testInput)).toBe(2286)
+})
 
-// test('part 2 actual', () => {
-//   expect(partTwo(input)).toBe()
-// })
+test('part 2 actual', () => {
+  expect(partTwo(input)).toBe(71036)
+})
--- years/2023/day-02/index.js
+++ years/2023/day-02/index.js
@@ -40,6 +40,45 @@ const partOne = (input) => {
   return filteredGames
 }
 
-const partTwo = (input) => {}
+const partTwoBaseObject = {
+  red: 0,
+  green: 0,
+  blue: 0,
+}
+
+const partTwo = (input) => {
+  const games = input.split('\n').map((line, index) => {
+    const [, gameRaw] = line.split(': ')
+    const gameId = index + 1
+
+    const gameParsed = gameRaw.split('; ').map(game => {
+      return game.split(', ').reduce((acc, set) => {
+        const [actColor, value] = set.split(' ').reverse()
+        return {
+          ...acc,
+          [actColor]: Number(value),
+        }
+      }, {})
+    }).reduce((acc, val) => {
+      const newMaximums = {}
+
+      if (val.red && val.red > acc.red) newMaximums.red = val.red
+      if (val.green && val.green > acc.green) newMaximums.green = val.green
+      if (val.blue && val.blue > acc.blue) newMaximums.blue = val.blue
+      return {
+        ...acc,
+        ...newMaximums,
+      }
+    }, partTwoBaseObject)
+    return {
+      gameId,
+      sets: gameParsed,
+    }
+  }).reduce((acc, { sets }) => {
+    return acc + (sets.red * sets.green * sets.blue)
+  }, 0)
+
+  return games
+}
 
 module.exports = { partOne, partTwo }
