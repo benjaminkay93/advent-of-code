be4b6fb92e09c2146b935d3deecbe28b6c002c09
Benjamin Kay <github@bkay.uk>
2024-12-02 11:10:33 +0000

    2024 day 1 complete

--- years/2024/day-01/day-01.test.js
+++ years/2024/day-01/day-01.test.js
@@ -2,21 +2,31 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(11)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(1258579)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(31)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(23981443)
 })
--- years/2024/day-01/index.js
+++ years/2024/day-01/index.js
@@ -1,5 +1,38 @@
-const partOne = (input) => {}
+const partOne = (input) => {
+  const parsedInput = input.split('\n').reduce((acc, line) => {
+    const [left, right] = line.split('   ')
+    return {
+      left: [...acc.left, parseInt(left)],
+      right: [...acc.right, parseInt(right)],
+    }
+  }, { left: [], right: [] })
 
-const partTwo = (input) => {}
+  const sortedLeft = parsedInput.left.sort((a, b) => a - b)
+  const sortedRight = parsedInput.right.sort((a, b) => a - b)
+  const diffArray = sortedLeft.map((left, index) => {
+    if (sortedRight[index] > left) return sortedRight[index] - left
+    return left - sortedRight[index]
+  }).reduce((acc, val) => acc + val, 0)
+  return diffArray
+}
+
+const partTwo = (input) => {
+  const parsedInput = input.split('\n').reduce((acc, line) => {
+    const [left, right] = line.split('   ')
+    return {
+      left: [...acc.left, parseInt(left)],
+      right: [...acc.right, parseInt(right)],
+    }
+  }, { left: [], right: [] })
+  const rightCount = parsedInput.right.reduce((acc, val) => {
+    if (acc[val]) return { ...acc, [val]: acc[val] + 1 }
+    return { ...acc, [val]: 1 }
+  }, {})
+  const leftCount = parsedInput.left.reduce((acc, val) => {
+    if (rightCount[val]) return acc + (rightCount[val] * val)
+    return acc
+  }, 0)
+  return leftCount
+}
 
 module.exports = { partOne, partTwo }
