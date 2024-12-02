e578721b3b1dd5bd2e03d18d335eb05db605c66c
Benjamin Kay <github@bkay.uk>
2024-12-02 13:51:22 +0000

    2024 day 2 complete

--- years/2024/day-02/day-02.test.js
+++ years/2024/day-02/day-02.test.js
@@ -2,21 +2,31 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(2)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(359)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(4)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(418)
 })
--- years/2024/day-02/index.js
+++ years/2024/day-02/index.js
@@ -1,5 +1,62 @@
-const partOne = (input) => {}
+const testRow = (row) => {
+  const increasing = row[0] < row[1]
+  if (increasing) {
+    return row.reduce((acc, num, i) => {
+      if (acc) return acc
+      if (i === row.length - 1) return acc
+      // look ahead decreasing
+      if (num > row[i + 1]) return true
+      // look ahead is the same value
+      if (num === row[i + 1]) return true
+      // look ahead increasing by more than 3
+      if ((row[i + 1] - num) > 3) return true
+      return false
+    }, false)
+  }
 
-const partTwo = (input) => {}
+  return row.reduce((acc, num, i) => {
+    if (acc) return acc
+    if (i === row.length - 1) return acc
+    // look ahead decreasing
+    if (num < row[i + 1]) return true
+    // look ahead is the same value
+    if (num === row[i + 1]) return true
+    // look ahead increasing by more than 3
+    if ((num - row[i + 1]) > 3) return true
+    return false
+  }, false)
+}
+
+const partOne = (input) => {
+  const parsedInput = input.split('\n').map((rawRow) => {
+    const row = rawRow.split(' ').map((num) => parseInt(num))
+    return testRow(row)
+  }).reduce((acc, val) => {
+    if (val) return acc
+    return acc + 1
+  }, 0)
+  return parsedInput
+}
+const testRowPartTwo = (row) => {
+  const testCases = row.length
+  let successes = 0
+  for (let i = 0; i < testCases; i++) {
+    const newRow = row.filter((num, j) => j !== i)
+    const result = testRow(newRow)
+    if (!result) successes += 1
+  }
+  return successes
+}
+
+const partTwo = (input) => {
+  const parsedInput = input.split('\n').map((rawRow) => {
+    const row = rawRow.split(' ').map((num) => parseInt(num))
+    return testRowPartTwo(row)
+  }).reduce((acc, val) => {
+    if (val === 0) return acc
+    return acc + 1
+  }, 0)
+  return parsedInput
+}
 
 module.exports = { partOne, partTwo }
