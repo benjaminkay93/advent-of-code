dbd0957bfce6e58d92f9c2d402c147257176cd51
Benjamin Kay <github@bkay.uk>
2024-12-03 09:57:14 +0000

    2024 day 3 complete

--- years/2024/day-03/day-03.test.js
+++ years/2024/day-03/day-03.test.js
@@ -2,19 +2,19 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(161)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(159892596)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(48)
 })
 
 test('part 2 actual', () => {
--- years/2024/day-03/index.js
+++ years/2024/day-03/index.js
@@ -1,5 +1,32 @@
-const partOne = (input) => {}
+const regex = /mul\((\d*,\d*)\)/gis
 
-const partTwo = (input) => {}
+const partOne = (input) => {
+  const result = input.match(regex)
+  return result.map(r => {
+    const [first, second] = r.replace('mul(', '').replace(')', '').split(',')
+    return first * second
+  }).reduce((acc, n) => acc + n, 0)
+}
+
+const partTwoRegex = /mul\((\d*,\d*)\)|don't\(\)|do\(\)/gis
+const partTwo = (input) => {
+  let doNotProcess = false
+  const result = input.match(partTwoRegex).filter(r => {
+    if (r === "do()") {
+      doNotProcess = false
+      return false
+    }
+    if (r === "don't()") {
+      doNotProcess = true
+      return false
+    }
+    return !doNotProcess
+  }).map(r => {
+    const [first, second] = r.replace('mul(', '').replace(')', '').split(',')
+    return first * second
+  }).reduce((acc, n) => acc + n, 0)
+
+  return result
+}
 
 module.exports = { partOne, partTwo }
