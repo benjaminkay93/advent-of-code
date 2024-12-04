4e4b2838bcd8d73270e89c744a381a28cafae6f8
Benjamin Kay <github@bkay.uk>
2024-12-04 15:49:53 +0000

    2024 day 4 complete

--- years/2024/day-04/day-04.test.js
+++ years/2024/day-04/day-04.test.js
@@ -2,21 +2,39 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(18)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(2545)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(9)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(1886)
 })
--- years/2024/day-04/index.js
+++ years/2024/day-04/index.js
@@ -1,5 +1,77 @@
-const partOne = (input) => {}
+const partOne = (input) => {
+  const dict = {}
+  input.split('\n').forEach((line, lineIndex) => line.split('').forEach((char, charIndex) => {
+    dict[`${charIndex},${lineIndex}`] = char
+  }))
 
-const partTwo = (input) => {}
+  let matchingCount = 0;
+
+  Object.keys(dict).forEach((key) => {
+    const [x, y] = key.split(',').map(val => parseInt(val, 10))
+
+    if (dict[`${x},${y}`] !== 'X') return
+
+    if (dict?.[`${x+1},${y}`] === 'M' && dict?.[`${x+2},${y}`] === 'A' && dict?.[`${x+3},${y}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+    if (dict?.[`${x+1},${y+1}`] === 'M' && dict?.[`${x+2},${y+2}`] === 'A' && dict?.[`${x+3},${y+3}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+    if (dict?.[`${x},${y+1}`] === 'M' && dict?.[`${x},${y+2}`] === 'A' && dict?.[`${x},${y+3}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+    if (dict?.[`${x-1},${y+1}`] === 'M' && dict?.[`${x-2},${y+2}`] === 'A' && dict?.[`${x-3},${y+3}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+    if (dict?.[`${x-1},${y}`] === 'M' && dict?.[`${x-2},${y}`] === 'A' && dict?.[`${x-3},${y}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+    if (dict?.[`${x-1},${y-1}`] === 'M' && dict?.[`${x-2},${y-2}`] === 'A' && dict?.[`${x-3},${y-3}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+    if (dict?.[`${x},${y-1}`] === 'M' && dict?.[`${x},${y-2}`] === 'A' && dict?.[`${x},${y-3}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+    if (dict?.[`${x+1},${y-1}`] === 'M' && dict?.[`${x+2},${y-2}`] === 'A' && dict?.[`${x+3},${y-3}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+  })
+
+  return matchingCount
+}
+
+const partTwo = (input) => {
+
+  const dict = {}
+  input.split('\n').forEach((line, lineIndex) => line.split('').forEach((char, charIndex) => {
+    dict[`${charIndex},${lineIndex}`] = char
+  }))
+
+  let matchingCount = 0;
+
+  Object.keys(dict).forEach((key) => {
+    const [x, y] = key.split(',').map(val => parseInt(val, 10))
+
+    if (dict[`${x},${y}`] !== 'A') return
+
+    if (dict?.[`${x+1},${y+1}`] === 'M' && dict?.[`${x-1},${y-1}`] === 'S' && dict?.[`${x+1},${y-1}`] === 'M' && dict?.[`${x-1},${y+1}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+
+    if (dict?.[`${x+1},${y+1}`] === 'S' && dict?.[`${x-1},${y-1}`] === 'M' && dict?.[`${x+1},${y-1}`] === 'M' && dict?.[`${x-1},${y+1}`] === 'S') {
+      matchingCount = matchingCount + 1
+    }
+
+    if (dict?.[`${x+1},${y+1}`] === 'M' && dict?.[`${x-1},${y-1}`] === 'S' && dict?.[`${x+1},${y-1}`] === 'S' && dict?.[`${x-1},${y+1}`] === 'M') {
+      matchingCount = matchingCount + 1
+    }
+
+    if (dict?.[`${x+1},${y+1}`] === 'S' && dict?.[`${x-1},${y-1}`] === 'M' && dict?.[`${x+1},${y-1}`] === 'S' && dict?.[`${x-1},${y+1}`] === 'M') {
+      matchingCount = matchingCount + 1
+    }
+  })
+
+  return matchingCount
+}
 
 module.exports = { partOne, partTwo }
