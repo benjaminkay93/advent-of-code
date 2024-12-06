61979e95fc99c6f3ea253508bdac78d6e3cee01d
Benjamin Kay <github@bkay.uk>
2024-12-06 12:58:25 +0000

    2024 day 5 complete

--- years/2024/day-05/day-05.test.js
+++ years/2024/day-05/day-05.test.js
@@ -2,21 +2,75 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
 
-  expect(partOne(testInput)).toBe()
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+  expect(partOne(testInput)).toBe(143)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(6498)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
 
-  expect(partTwo(testInput)).toBe()
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+
+  expect(partTwo(testInput)).toBe(123)
 })
 
 test('part 2 actual', () => {
-  expect(partTwo(input)).toBe()
+  expect(partTwo(input)).toBe(5017)
 })
--- years/2024/day-05/index.js
+++ years/2024/day-05/index.js
@@ -1,5 +1,113 @@
-const partOne = (input) => {}
+const partOne = (input) => {
+  const rules = {}
 
-const partTwo = (input) => {}
+  const [, messages] = input.split('\n\n').map((part, index) => part.split('\n').map(line => {
+    if (index === 0) {
+      const [before, after] = line.split('|')
+      if (rules[before]) {
+        rules[before].push(after)
+        return []
+      }
+      rules[before] = [after]
+      return []
+    }
+    if (index === 1) {
+      return line.split(',')
+    }
+    return []
+  }))
+
+  const validMessages = messages.filter(message => {
+    let valid = true
+    message.forEach((item, index) => {
+      for (let i = 0; i < index; i++) {
+        if (!rules[item]) {
+          break
+        }
+        if (rules[item].includes(message[i])) {
+          valid = false
+          break
+        }
+      }
+    })
+    return valid
+  })
+
+  return validMessages.reduce((acc, message) => {
+    return acc + parseInt(message[Math.ceil(message.length / 2) - 1])
+  }, 0)
+}
+
+const swapAndRevalidate = (rules, message) => {
+  const validatedMessage = [...message]
+  validatedMessage.forEach((item, index) => {
+    for (let i = 0; i < index; i++) {
+      if (!rules[item]) {
+        break
+      }
+      if (rules[item].includes(validatedMessage[i])) {
+        validatedMessage[index] = validatedMessage[i]
+        validatedMessage[i] = item
+        break
+      }
+    }
+  })
+  let isAllNotValid = false
+  validatedMessage.forEach((item, index) => {
+    for (let i = 0; i < index; i++) {
+      if (!rules[item]) {
+        break
+      }
+      if (rules[item].includes(validatedMessage[i])) {
+        isAllNotValid = true
+        break
+      }
+    }
+  })
+  if (isAllNotValid) {
+    return swapAndRevalidate(rules, validatedMessage)
+  }
+  return validatedMessage
+}
+
+const partTwo = (input) => {
+  const rules = {}
+
+  const [, messages] = input.split('\n\n').map((part, index) => part.split('\n').map(line => {
+    if (index === 0) {
+      const [before, after] = line.split('|')
+      if (rules[before]) {
+        rules[before].push(after)
+        return []
+      }
+      rules[before] = [after]
+      return []
+    }
+    if (index === 1) {
+      return line.split(',')
+    }
+    return []
+  }))
+
+  const invalidMessages = messages.filter(message => {
+    let valid = false
+    message.forEach((item, index) => {
+      for (let i = 0; i < index; i++) {
+        if (!rules[item]) {
+          break
+        }
+        if (rules[item].includes(message[i])) {
+          valid = true
+          break
+        }
+      }
+    })
+    return valid
+  })
+
+  return invalidMessages.map(message => swapAndRevalidate(rules, message)).reduce((acc, message) => {
+    return acc + parseInt(message[Math.ceil(message.length / 2) - 1])
+  }, 0)
+}
 
 module.exports = { partOne, partTwo }
