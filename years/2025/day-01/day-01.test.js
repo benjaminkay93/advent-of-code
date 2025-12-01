c68952f347fbcf2305bce8884179828a38c3cb2d
Benjamin Kay <github@bkay.uk>
2025-12-01 13:54:45 +0000

    update day 1

--- years/2025/day-01/day-01.test.js
+++ years/2025/day-01/day-01.test.js
@@ -2,21 +2,40 @@ const { partOne, partTwo } = require('.')
 const { input } = require('./input')
 
 test('part 1', () => {
-  const testInput = ``
+  const testInput = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
 
-  expect(partOne(testInput)).toBe()
+  expect(partOne(testInput)).toBe(3)
 })
 
 test('part 1 actual', () => {
-  expect(partOne(input)).toBe()
+  expect(partOne(input)).toBe(989)
 })
 
 test('part 2', () => {
-  const testInput = ``
+  const testInput = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
 
-  expect(partTwo(testInput)).toBe()
+  expect(partTwo(testInput)).toBe(6)
 })
 
 test('part 2 actual', () => {
+  // 5949 too high
   expect(partTwo(input)).toBe()
 })
--- years/2025/day-01/index.js
+++ years/2025/day-01/index.js
@@ -1,5 +1,61 @@
-const partOne = (input) => {}
+const directionParser = (line) => {
+  if (line.charAt(0) === 'L') {
+    return -Number(line.slice(1))
+  }
+  return Number(line.slice(1))
+}
 
-const partTwo = (input) => {}
+const getWrappedValue = (value) => {
+  if (value < 0) return getWrappedValue(value + 100)
+  if (value >= 100) return getWrappedValue(value - 100)
+  return value
+}
+const partOne = (input) => {
+  let count = 0
+  input.split('\n').reduce((acc, line) => {
+    const value = directionParser(line)
+    const newValue = acc + value
+
+    const wrapedValue = getWrappedValue(newValue)
+    if (wrapedValue === 0) {
+      count++
+    }
+    return wrapedValue
+  }, 50)
+  return count
+}
+
+const partTwo = (input) => {
+  let count = 0
+
+  const getWrappedValuePartTwo = (value) => {
+    if (value === 0) {
+      count++
+      return 0
+    }
+    if (value === 100) {
+      count++
+      return 0
+    }
+    if (value < 0) {
+      count++
+      return getWrappedValuePartTwo(value + 100)
+    }
+    if (value >= 100) {
+      count++
+      return getWrappedValuePartTwo(value - 100)
+    }
+    return value
+  }
+
+  input.split('\n').reduce((acc, line) => {
+    const value = directionParser(line)
+    const newValue = acc + value
+
+    const wrapedValue = getWrappedValuePartTwo(newValue)
+    return wrapedValue
+  }, 50)
+  return count
+}
 
 module.exports = { partOne, partTwo }
